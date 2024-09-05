2720213bf703e5b42f640e480fc6ac8a89d4dc27
Physician Techie <medtechvj@gmail.com>
2024-09-05 11:24:49 +0800

    initial commit of my python script

---
+++ geeks for geeks.py
@@ -0,0 +1,78 @@
+# geeks for geeks learning python
+
+"""
+print("hello world")
+
+# commenting
+# this is a comment in python
+print("hello world")  # this is also a comment in python
+
+# multiline comment
+
+this is a
+#multiline comment
+in python
+
+print("hello world")  # this is not a comment in python
+
+# variable declaration
+# in python we don't need to declare the type of variable before using it
+x = 10  # here x is a variable of type integer
+# print(x)  # this will print 10
+# print(type(x))  # this will print <class 'int'>
+# print(type("hello"))  # this will print <class 'str'>
+# print(type(10.5))  # this will print <class 'float'>
+# print(type(True))  # this will print <class 'bool'>
+# print(type(None))  # this will print <class 'NoneType'>
+# print(type([1, 2, 3]))  # this will print <class '
+# print(type({"a": 1, "b": 2}))  # this will print
+# <class 'dict'>
+# print(type((1, 2, 3)))  # this will print <class '
+# print(type({"a": 1, "b": 2}))  # this will print
+# <class 'dict'>
+# print(type({"a": 1, "b": 2}))  # this will print
+# <class 'dict'>
+# print(type({"a": 1, "b": 2}))  # this will print
+
+# Python data types
+x = 3j #complex
+# create the list
+my_list = [1, 2, 3, 4, 5]
+# create the tuple
+my_tuple = (1, 2, 3, 4, 5)
+# create the dictionary
+my_dict = {"a": 1, "b": 2, "c": 3}
+#create set
+my_set = {1, 2, 3, 4, 5}
+
+val = input("enter your value : ")
+print(val)
+
+# python if else
+i = 20
+if (i < 15):
+            print("i is less than 15")
+            print("i'm in if block")
+else:
+            print("i is greater than 15")
+            print("i'm outside the else block")
+print("i'm outside the if else block")
+
+
+for i in range (0,10,2):
+            print(i)
+
+"""
+
+
+
+def evenOdd(x):
+            if x % 2 == 0:
+                        return "Even"
+            else:
+                        return "Odd"
+
+print (evenOdd(2))
+print (evenOdd(5))
+
+
