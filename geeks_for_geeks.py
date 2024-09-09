8102692084dc694cfff12b8ac3fb0ea4d42ba67b
Physician Techie <medtechvj@gmail.com>
2024-09-09 15:16:19 +0800

    9 sep 2024

--- geeks_for_geeks.py
+++ geeks_for_geeks.py
@@ -195,5 +195,15 @@ class CSStudent:
 '''
 
 #####################################################
-Learn Python Input/Output
+#Learn Python Input/Output
 ###################################################
+# build a countdown timer like 5>4>3>2>1
+import time
+
+for i in range(5, 0, -1):
+            print(i)
+            time.sleep(1)
+print("blast off!")
+
+
+
