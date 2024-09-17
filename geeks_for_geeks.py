ab3ec5398b9f97fb5a85b49185c55ec029ce3742
Physician Techie <medtechvj@gmail.com>
2024-09-17 16:22:48 +0800

    17 sep 2024 work completed

--- geeks_for_geeks.py
+++ geeks_for_geeks.py
@@ -1,12 +1,6 @@
 # geeks for geeks learning python
-#Python exception handling
-# Python program to handle simple runtime error
-a = [1, 2, 3]
-try:
-    print("Second element = %d" %(a[1]))
-    
-    # Throws error since there are only 3 elements in array
-    print("Fourth element = %d" %(a[3]))
-except IndexError:
-    print("An error occurred")
-       
+
+# Python file handling
+# File handling is an important part of web application.
+# Python has several functions for creating, reading, updating, and deleting files.
+# Python's file handling is based on the concept of a file object.
